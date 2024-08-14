5cb547200de8462a4f65269682a9c1eee91c2e4b
Volodymyr Hrytsaienko <3q3q18@gmail.com>
2024-08-14 15:24:46 +0200

    first commit

--- infra/version.tf
+++ infra/version.tf
@@ -1,6 +1,9 @@
 terraform {
   required_version = ">= 1.6.4"
 
+  backend "local" {
+    path = "./infra/terraform.tfstate"
+
   required_providers {
     github = {
       source  = "integrations/github"
